9b3a03f7f3c20d6bd49d312a3f31a9cc6d8672af
kshivanku <kshivanku@gmail.com>
2018-01-10 23:59:19 -0500

    changes

--- index.js
+++ index.js
@@ -1,173 +1,180 @@
 var request = require('request');
 var fs = require('fs');
 var json2csv = require('json2csv');
-
+var keyword_extractor = require("keyword-extractor");
 
 var headers = {
     'Authorization': 'OAuth A30C8sKL4ofzHS-z_JdLhIyiR2xYQaL312jqK8aTxOOw2C4zpO7mxEyy5MQmhD07s668l1rU3XerILe35EAmi2WMz32h7VGN_r06R_BYIHCQFFlFCkX2Px2rB68LKBc5vNTN7ingPXGr6Fe60toum2F_vyzgHsxfLM475CsOKdn4KrIUyaddqk1-58nXb1qReAY13JhSRwPMRpCCguT3zkk2CNgM0b6zXHoC_ffHJXljl0kJHCLyoi9p_pNW:feedlydev'
 };
-var request_url = 'http://cloud.feedly.com/v3/streams/contents?streamId=feed%2Fhttp%3A%2F%2Fwww.firstpost.com%2Ffeed%2Frss&count=100';
+var request_url = 'http://cloud.feedly.com/v3/subscriptions';
 var request_method = 'GET';
 var request_body = null;
 
 //MAKE THE REQUEST
 // makeTheRequest();
-function makeTheRequest(){
-  var options = {
-      url: request_url,
-      headers: headers,
-      method: request_method,
-      body: request_body
-  };
+function makeTheRequest() {
+    var options = {
+        url: request_url,
+        headers: headers,
+        method: request_method,
+        body: request_body
+    };
 
-  request(options, callback);
+    request(options, callback);
 
-  function callback(error, response, body) {
-      console.log(response.statusCode)
-      if (!error && response.statusCode == 200) {
-          body_json = JSON.parse(body)
-          // return(body_json);
-          fs.writeFileSync('allArticles_firstpost.json', JSON.stringify(body_json, null, 2))
-      }
-      else {
-        console.log(error)
-      }
-  }
+    function callback(error, response, body) {
+        console.log(response.statusCode)
+        if (!error && response.statusCode == 200) {
+            body_json = JSON.parse(body)
+            // return(body_json);
+            fs.writeFileSync('subscriptions.json', JSON.stringify(body_json, null, 2))
+        } else {
+            console.log(error)
+        }
+    }
 }
-
 
 //FIND MOST ACTIVE SOURCES
 // findMostActiveSources();
-function findMostActiveSources(){
-  var file_data = JSON.parse(fs.readFileSync('subscriptions.json'));
-  var allvelocities = {};
-  for(var i = 0 ; i < file_data.length ; i++) {
-    if(file_data[i].topics) {
-      for(var j = 0 ; j < file_data[i].topics.length ; j++) {
-        if(file_data[i].topics[j].toLowerCase() == 'news' || file_data[i].topics[j].toLowerCase() == 'politics') {
-          allvelocities[file_data[i].title] = file_data[i].velocity;
-        }
-      }
+function findMostActiveSources() {
+    var file_data = JSON.parse(fs.readFileSync('subscriptions.json'));
+    var allvelocities = {};
+    for (var i = 0; i < file_data.length; i++) {
+        // if (file_data[i].topics) {
+            // for (var j = 0; j < file_data[i].topics.length; j++) {
+                // if (file_data[i].topics[j].toLowerCase() == 'news' || file_data[i].topics[j].toLowerCase() == 'politics') {
+                    allvelocities[file_data[i].title] = file_data[i].velocity;
+                // }
+            // }
+        // }
     }
-  }
-  fs.writeFileSync('allVelocities_news.json', JSON.stringify(sortObject(allvelocities), null, 2));
+    fs.writeFileSync('allVelocities.json', JSON.stringify(sortObject(allvelocities), null, 2));
 }
-
 
 //FIND ALL TOPICS
 // findalltopics();
-function findalltopics(){
-  var file_data = JSON.parse(fs.readFileSync('subscriptions.json'));
-  var allTopics = {};
-  for(var i = 0 ; i < file_data.length ; i++) {
-    if(file_data[i].topics) {
-      for(var j = 0 ; j < file_data[i].topics.length ; j++){
-        if(allTopics[file_data[i].topics[j]]){
-          allTopics[file_data[i].topics[j]] += 1;
+function findalltopics() {
+    var file_data = JSON.parse(fs.readFileSync('subscriptions.json'));
+    var allTopics = {};
+    for (var i = 0; i < file_data.length; i++) {
+        if (file_data[i].topics) {
+            for (var j = 0; j < file_data[i].topics.length; j++) {
+                if (allTopics[file_data[i].topics[j]]) {
+                    allTopics[file_data[i].topics[j]] += 1;
+                } else {
+                    allTopics[file_data[i].topics[j]] = 1;
+                }
+            }
+        } else {
+            console.log(file_data[i].title);
         }
-        else {
-          allTopics[file_data[i].topics[j]] = 1;
-        }
-      }
     }
-    else{
-      console.log(file_data[i].title);
-    }
-  }
-  fs.writeFileSync('allTopics.json', JSON.stringify(sortObject(allTopics), null, 2));
+    fs.writeFileSync('allTopics.json', JSON.stringify(sortObject(allTopics), null, 2));
 }
-
 
 //FIND SOURCES BY TOPIC
 // findSourcesByTopic('news');
-function findSourcesByTopic(topic_id){
-  var file_data = JSON.parse(fs.readFileSync('subscriptions.json'));
-  var sources = [];
-  for (var i = 0; i < file_data.length ; i++) {
-    if(file_data[i].topics) {
-      for(var j = 0 ; j < file_data[i].topics.length ; j++) {
-        if(file_data[i].topics[j] == topic_id) {
-          sources.push(file_data[i].title)
+function findSourcesByTopic(topic_id) {
+    var file_data = JSON.parse(fs.readFileSync('subscriptions.json'));
+    var sources = [];
+    for (var i = 0; i < file_data.length; i++) {
+        if (file_data[i].topics) {
+            for (var j = 0; j < file_data[i].topics.length; j++) {
+                if (file_data[i].topics[j] == topic_id) {
+                    sources.push(file_data[i].title)
+                }
+            }
         }
-      }
     }
-  }
-  fs.writeFileSync('sourcesByTopic.json', JSON.stringify(sources, null, 2));
+    fs.writeFileSync('sourcesByTopic.json', JSON.stringify(sources, null, 2));
 }
 
-
-
-function sortObject(obj){
-  var sortable = [];
-  for (var item in obj){
-    sortable.push([item, obj[item]])
-  }
-  sortable.sort(function(a,b){
-    return b[1] - a[1];
-  })
-  return sortable;
+function sortObject(obj) {
+    var sortable = [];
+    for (var item in obj) {
+        sortable.push([item, obj[item]])
+    }
+    sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    })
+    return sortable;
 }
 
 //HEADLINE EXTRACTOR
-// headlineExtractor('allArticles_firstpost.json');
-function headlineExtractor(file_name){
-  var file_data = JSON.parse(fs.readFileSync(file_name));
-  var headlines = [];
-  for(var i = 0; i < file_data.items.length ; i++){
-    headlines.push(file_data.items[i].title);
-  }
-  fs.writeFileSync('headlines_firstpost.json', JSON.stringify(headlines, null, 2));
+// headlineExtractor('allArticles_fox.json');
+function headlineExtractor(file_name) {
+    var file_data = JSON.parse(fs.readFileSync(file_name));
+    var headlines = [];
+    for (var i = 0; i < file_data.items.length; i++) {
+        headlines.push(file_data.items[i].title);
+    }
+    fs.writeFileSync('headlines_fox.json', JSON.stringify(headlines, null, 2));
 }
 
-
-jsonToCSV();
-function jsonToCSV(){
-  var fields = ['publication', 'velocity']
-  var allVelocitiesArray = JSON.parse(fs.readFileSync('allVelocities_news.json'))
-  var allVelocities = [];
-  for (var i = 0 ; i < allVelocitiesArray.length ; i++){
-    var key_val_pair = {}
-    key_val_pair['publication'] = allVelocitiesArray[i][0];
-    key_val_pair['velocity'] = allVelocitiesArray[i][1];
-    allVelocities.push(key_val_pair);
-  }
-  var csv = json2csv({ data: allVelocities, fields: fields });
-  fs.writeFile('allVelocities_news.csv', csv, function(err) {
-    if (err) throw err;
-    console.log('file saved');
-  });
+// jsonToCSV();
+function jsonToCSV() {
+    var fields = ['publication', 'velocity']
+    var allVelocitiesArray = JSON.parse(fs.readFileSync('allVelocities.json'))
+    var allVelocities = [];
+    for (var i = 0; i < allVelocitiesArray.length; i++) {
+        var key_val_pair = {}
+        key_val_pair['publication'] = allVelocitiesArray[i][0];
+        key_val_pair['velocity'] = allVelocitiesArray[i][1];
+        allVelocities.push(key_val_pair);
+    }
+    var csv = json2csv({data: allVelocities, fields: fields});
+    fs.writeFile('allVelocities.csv', csv, function(err) {
+        if (err)
+            throw err;
+        console.log('file saved');
+    });
 }
 
+//KEYWORD EXTRACTOR
+// keywords('headlines_fox.json')
+function keywords(file_name) {
+    var headlinesArray = JSON.parse(fs.readFileSync(file_name));
+    pub_keywords = {}
+    for (var i = 0; i < headlinesArray.length; i++) {
+        var sentence = headlinesArray[i];
+        var extraction_result = keyword_extractor.extract(sentence, {
+            language: "english",
+            remove_digits: true,
+            return_changed_case: true,
+            remove_duplicates: false
 
-
-
-
-
-
-
-
-
+        });
+        for(var j = 0 ; j < extraction_result.length; j++) {
+          if(pub_keywords[extraction_result[j]]) {
+            pub_keywords[extraction_result[j]] += 1;
+          }
+          else {
+            pub_keywords[extraction_result[j]] = 1;
+          }
+        }
+    }
+    fs.writeFileSync('keywords_fox.json', JSON.stringify(sortObject(pub_keywords), null, 2))
+}
 
 //   if(file_data[i].state){
 //     feedIDs.push(file_data[i].id);
 //     console.log(file_data[i].title);
 //   }
-  // if(i == file_data.length-1) {
-  //   var headers = {
-  //       'Authorization': 'OAuth A30C8sKL4ofzHS-z_JdLhIyiR2xYQaL312jqK8aTxOOw2C4zpO7mxEyy5MQmhD07s668l1rU3XerILe35EAmi2WMz32h7VGN_r06R_BYIHCQFFlFCkX2Px2rB68LKBc5vNTN7ingPXGr6Fe60toum2F_vyzgHsxfLM475CsOKdn4KrIUyaddqk1-58nXb1qReAY13JhSRwPMRpCCguT3zkk2CNgM0b6zXHoC_ffHJXljl0kJHCLyoi9p_pNW:feedlydev'
-  //   };
-  //
-  //   var options = {
-  //       url: 'http://cloud.feedly.com/v3/subscriptions/.mdelete',
-  //       headers: headers,
-  //       method: 'DELETE',
-  //       body: feedIDs,
-  //       json: true
-  //   };
-  //
-  //   request(options, callback);
-  // }
-
+// if(i == file_data.length-1) {
+//   var headers = {
+//       'Authorization': 'OAuth A30C8sKL4ofzHS-z_JdLhIyiR2xYQaL312jqK8aTxOOw2C4zpO7mxEyy5MQmhD07s668l1rU3XerILe35EAmi2WMz32h7VGN_r06R_BYIHCQFFlFCkX2Px2rB68LKBc5vNTN7ingPXGr6Fe60toum2F_vyzgHsxfLM475CsOKdn4KrIUyaddqk1-58nXb1qReAY13JhSRwPMRpCCguT3zkk2CNgM0b6zXHoC_ffHJXljl0kJHCLyoi9p_pNW:feedlydev'
+//   };
+//
+//   var options = {
+//       url: 'http://cloud.feedly.com/v3/subscriptions/.mdelete',
+//       headers: headers,
+//       method: 'DELETE',
+//       body: feedIDs,
+//       json: true
+//   };
+//
+//   request(options, callback);
+// }
 
 /*
 OTHER REQUESTS
